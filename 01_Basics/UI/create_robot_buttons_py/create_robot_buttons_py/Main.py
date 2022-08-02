34bff67e8fff47f1e09252cc82d5154020a74cb7
bva.bmkr <bva.bmkr@gmail.com>
2022-08-02 11:26:47 +0200

    --button subscriber added (test)

---
+++ 01_Basics/UI/create_robot_buttons_py/create_robot_buttons_py/Main.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+
+from irobot_create_msgs.msg import InterfaceButtons
+
+
+class ButtonSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(InterfaceButtons,'interface_buttons',self.button_callback,10)
+        self.subscription  # prevent unused variable warning
+
+    def button_callback(self, msg):
+        #identification of the pushed button
+        if msg.button_1.is_pressed:
+            self.get_logger().info('Button 1 pressed for "%s" nanoseconds' % msg.button_1.last_pressed_duration.__str__())
+        if msg.button_2.is_pressed:
+            self.get_logger().info('Button 2 pressed for "%s" nanoseconds' % msg.button_1.last_pressed_duration.__str__())
+        if msg.button_power.is_pressed:
+            self.get_logger().info('Button power pressed for "%s" nanoseconds' % msg.button_1.last_pressed_duration.__str__())
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = ButtonSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 01_Basics/UI/create_robot_buttons_py/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'create_robot_buttons_py'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Van Acker Bert',
+    author_email='bva.bmkr@gmail.com',
+    maintainer='Van Acker Bert',
+    maintainer_email='bva.bmkr@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Getting started with the irobot create buttons',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'Buttons_basic = create_robot_buttons_py.Main:main'
+        ],
+    },
+)
