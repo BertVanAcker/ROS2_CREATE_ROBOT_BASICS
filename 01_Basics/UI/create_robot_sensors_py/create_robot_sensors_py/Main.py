abb4d815c15cf2597c46b6b1e99d4787ccffa1e8
bva.bmkr <bva.bmkr@gmail.com>
2022-08-04 14:35:52 +0200

    no message

--- 01_Basics/UI/create_robot_sensors_py/create_robot_sensors_py/Main.py
+++ 01_Basics/UI/create_robot_sensors_py/create_robot_sensors_py/Main.py
@@ -76,19 +76,19 @@ class ProximityDetector(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    hazardDetector = HazardDetector()
-    #proximityDetector = ProximityDetector()
+    #hazardDetector = HazardDetector()
+    proximityDetector = ProximityDetector()
     #kidnapDetector = KidnapDetector()
 
-    rclpy.spin(hazardDetector)
-    #rclpy.spin(proximityDetector)
+    #rclpy.spin(hazardDetector)
+    rclpy.spin(proximityDetector)
     #rclpy.spin(kidnapDetector)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    hazardDetector.destroy_node()
-    #proximityDetector.destroy_node()
+    #hazardDetector.destroy_node()
+    proximityDetector.destroy_node()
     #kidnapDetector.destroy_node()
     rclpy.shutdown()
 
