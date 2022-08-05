1df7f960dccace323dee9638b4b3862691a8c388
bva.bmkr <bva.bmkr@gmail.com>
2022-08-05 10:35:55 +0200

    Update node updated

--- 01_Basics/UI/create_robot_movement_py/UpdateNode.sh
+++ 01_Basics/UI/create_robot_movement_py/UpdateNode.sh
@@ -7,6 +7,6 @@ git pull
 #return to ROS2 base workspace
 cd ~/dev_ws/
 #build selected package
-colcon build -- packages-select create_robot_movement_py
+colcon build --packages-select create_robot_movement_py
 #source the installation
 source install/setup.bash
